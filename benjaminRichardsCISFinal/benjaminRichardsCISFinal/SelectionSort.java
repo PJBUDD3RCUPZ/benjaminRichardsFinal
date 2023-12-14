55cc50c81ba83a1c7d8568ba4dff5d64857485da
PJ <pjbuddercup@gmail.com>
2023-12-14 16:57:19 -0500

    Update SelectionSort.java

--- benjaminRichardsCISFinal/benjaminRichardsCISFinal/SelectionSort.java
+++ benjaminRichardsCISFinal/benjaminRichardsCISFinal/SelectionSort.java
@@ -1,23 +1,25 @@
 package benjaminRichardsCISFinal;
 
-public class SelectionSort {
-
-    public int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; ++i) {
-            //int minIndex = i;
-            for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[i] > arr[j]) {
-                    //minIndex = j;
-                    swap(arr, i, j);
-                }
+public void selectionSort(int[] arr) {
+    if (arr == null || arr.length <= 1) {
+        return;
+    }
+    for (int i = 0; i < arr.length - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
-		return arr;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        if (minIndex <= i) {
+            swap(arr, i, minIndex);
+        }
     }
 }
+
+private void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[j] = arr[i];
+    arr[j] = temp;
+}
+
